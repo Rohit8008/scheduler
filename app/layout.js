896183aaf657e6ce1f5073c53fa74f9667905ed2
Rohit Mittal <rohitmittal137@gmail.com>
2025-01-24 10:26:36 +0530

    create event completed

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import CreateEventDrawer from "@/components/CreateEvent";
 
 export const metadata = {
   title: "Scheduler",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
               <p>Made by Rohit Mittal</p>
             </div>
           </footer>
+          <CreateEventDrawer />
         </body>
       </html>
     </ClerkProvider>
--- app/lib/validators.js
+++ app/lib/validators.js
@@ -10,3 +10,15 @@ export const userNameSchema = z.object({
       "Username can only consist of letters,numbers and underscore(_)"
     ),
 });
+
+export const eventSchema = z.object({
+  title: z.string()
+    .min(1, "Title is Required")
+    .max(100, "Title must be 100 character or less"),
+  description: z.string()
+    .min(1, "Description is Required")
+    .max(500, "Description must be 500 character or less"),
+  duration: z.number().int().positive("Duration must be a positive number"),
+
+  isPrivate: z.boolean(),
+});
